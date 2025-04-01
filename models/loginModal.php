9b08b51f5aa09efb637c7638778fea1e6f77b284
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-03-31 22:19:03 -0600

    LoginModalDone

--- models/loginModal.php
+++ models/loginModal.php
@@ -15,7 +15,7 @@
         <div class="flex flex-col space-y-6 bg-white rounded-md max-w-md w-full mx-auto 
          md:flex-row md:max-w-3xl md:space-y-0 md:pr-0">
             <!-- Log in and x -->
-            <div class="flex flex-col space-y-4 p-6">
+            <div class="flex flex-col space-y-4 p-6 ">
                 <div class="flex justify-between text-2xl">
                     <h2>Log in</h2>
                     <p class="text-3xl text-[#4be6a4] md:hidden"><i class="fa-solid fa-circle-xmark"></i></p>
@@ -24,14 +24,16 @@
                 <p>Log in to your account to upload or download pictures,videos or music</p>
 
                 <div class="flex flex-col space-y-4  ">
-                    <div class="border border-solid border-[#cccc] py-4 px-4.5"><input type="text"
-                            placeholder="Enter your email address"></div>
-                    <div class="border border-solid border-[#cccc] py-4 px-4.5"><input type="text"
-                            placeholder="Enter your password"></div>
+                    <div class="border border-solid border-[#cccc] py-4 px-4.5"><input class="w-full focus:outline-none"
+                            type="text" placeholder="Enter your email address"></div>
+                    <div class="border border-solid border-[#cccc] py-4 px-4.5"><input class="w-full focus:outline-none"
+                            type="text" placeholder="Enter your password"></div>
                     <p class="text-[#015057] text-center">Forgot password</p>
                 </div>
 
-                <button class="bg-[#015057] text-white py-4 rounded-md gap-4 flex items-center justify-center">Next <p>
+                <button
+                    class="bg-[#015057] text-white py-4 rounded-md gap-4 flex items-center justify-center transition-all duration-500 hover:bg-[rgba(1,80,87,0.8)] hover:cursor-pointer hover:gap-6">Next
+                    <p>
                         <i class="fa-solid fa-arrow-right">
                     </p></i>
                 </button>
@@ -42,12 +44,12 @@
 
                 <div class="flex flex-col gap-4 items-center justify-center">
                     <button
-                        class="border border-solid border-[#cccc] py-4 px-8 w-full flex items-center justify-center "><img
+                        class="border border-solid border-[#cccc] py-4 px-8 w-full flex items-center justify-center hover:cursor-not-allowed "><img
                             class="w-9 mr-4" src="/logingModal/alojamientos/src/images/facebook.png" alt="">
                         <p>Facebook</p>
                     </button>
                     <button
-                        class="border border-solid border-[#cccc] py-4 px-8 w-full flex items-center justify-center"><img
+                        class="border border-solid border-[#cccc] py-4 px-8 w-full flex items-center justify-center hover:cursor-not-allowed"><img
                             class="w-9 mr-4" src="
                     /logingModal/alojamientos/src/images/google.png" alt="">
                         <p>Google</p>
@@ -65,8 +67,9 @@
                 <figure class="flex w-sm">
                     <img class="rounded-r-lg" src="/logingModal/alojamientos/src/images/image-1.jpg" alt="">
                 </figure>
-                <p class="absolute text-4xl -top-4 -right-4 text-[#343434] rounded-[50%] shadow-lg"><i
-                        class=" fa-solid fa-circle-xmark "></i></p>
+                <p
+                    class="absolute text-4xl -top-4 -right-4 text-[#343434] rounded-[50%] shadow-lg hover:cursor-pointer">
+                    <i class=" fa-solid fa-circle-xmark "></i></p>
             </div>
         </div>
     </div>
