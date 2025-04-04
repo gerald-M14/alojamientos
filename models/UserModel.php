d0b666ef952c3f8381812532f22e991e9f477db4
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-04-03 19:52:47 -0600

    Update

--- models/UserModel.php
+++ models/UserModel.php
@@ -1,6 +1,25 @@
 <?php
 
 class UsersModel{
+    public $id_user;
+    public $name;
+    public $email;
+    public $password;
+    
+    public $age;
+    public $role;
+    
+    public function __construct( $name, $email, $age, $role, $password)
+    {
+        
+        $this->name= $name;
+        $this->email= $email;
+        $this->age = $age;
+        $this->role =$role;
+        $this->password = $password; 
+    }
+
+    
     public static function all(){
         $pdo = Connection::getInstance()->getConnection();
         $query = $pdo->query("SELECT id_user, name FROM users");
@@ -20,6 +39,23 @@ class UsersModel{
         return $user;    
     }
 
-
+    public function save(){
+        $pdo = Connection::getInstance()->getConnection();
+        $query = $pdo->prepare("INSERT INTO users(name, email, password, age, id_role) VALUES (:name, :email, :pass, :age, :role)");
+        $result = $query->execute([ 
+            ':name' =>  $this->name,
+            ':email' => $this->email,
+            ':pass' => $this->password,
+            ':age' => $this->age,
+            ':role' => $this->role
+        ]);
+        return $result;
+        
+    }
+    
+    public static function create($name, $email, $password, $age, $role) {
+        $user = new self($name, $email, $password, $age, $role);
+        return $user->save();
+    }
 
 }
--- views/signinModal.php
+++ views/signinModal.php
@@ -22,45 +22,69 @@ require_once "../controller/LoginController.php";
     <!-- WRAPPER -->
     <div class="relative flex flex-col items-center justify-center min-h-screen 
     bg-[url('https://img.freepik.com/free-photo/composition-small-airplanes-bus-passport-map_23-2148169874.jpg')] 
-    bg-cover bg-center before:absolute before:inset-0 before:bg-[#1c4245] before:opacity-50 before:content-[''] z-0 ">
+    bg-cover bg-center before:absolute before:inset-0 before:bg-[#1c4245] before:opacity-50 before:content-[''] z-0">
 
 
 
 
         <!-- MAINCONTAINER -->
-        <div class="flex flex-col space-y-6 bg-white rounded-md my-4 max-w-sm w-full md:mx-auto 
+        <div class="flex flex-col space-y-6 bg-white rounded-md my-4  max-w-sm w-full mx-auto 
          md:flex-row md:max-w-3xl md:space-y-0 md:pr-0  z-10">
             <!-- Log in and x -->
             <div class="flex flex-col space-y-4 p-6 ">
                 <div class="flex justify-between text-2xl">
-                    <h2>Log in</h2>
+                    <h2>Sign in</h2>
                     <a href="./landingPage.php" class="text-3xl text-[#4be6a4] md:hidden"><i
                             class="fa-solid fa-circle-xmark"></i></a>
                 </div>
 
-                <p>Log in to your account to see your profile and check you favorites</p>
+                <p>Sign in and have access to a whole new world, hurry up now, the sooner the better.</p>
 
                 <!-- inputsFORM -->
                 <form action="" method="POST" class="flex flex-col">
                     <div class="flex flex-col space-y-4  ">
                         <div class="border border-solid border-[#cccc] py-4 px-4.5">
                             <!-- EMAIL -->
-                            <input class="w-full focus:outline-none" type="text" name="email"
-                                placeholder="Enter your email address">
+                            <input class="w-full focus:outline-none" type="text" name="name"
+                                placeholder="Enter your name">
                             <!--  -->
                         </div>
+                        <div class="border border-solid border-[#cccc] py-4 px-4.5">
+                            <!--PASSWORD  -->
+                            <input class="w-full focus:outline-none" type="text" name="email"
+                                placeholder="Enter your Email address">
+                            <!--  -->
+                        </div>
+
                         <div class="border border-solid border-[#cccc] py-4 px-4.5">
                             <!--PASSWORD  -->
                             <input class="w-full focus:outline-none" type="password" name="password"
                                 placeholder="Enter your password">
                             <!--  -->
-
                         </div>
-                        <p class="text-[#015057] text-center mb-4">Forgot password</p>
+
+                        <div class="border border-solid border-[#cccc] py-4 px-4.5">
+                            <!--PASSWORD  -->
+                            <input class="w-full focus:outline-none" type="text" name="age"
+                                placeholder="Enter your Age">
+                            <!--  -->
+                        </div>
+
+                        <div class="border border-solid border-[#cccc] py-4 px-4.5">
+                            <!--PASSWORD  -->
+                            <select>
+                                <option value="1">User</option>
+                                <option value="2">Admin</option>
+                            </select>
+                            <!--  -->
+                        </div>
+
+
+
                     </div>
-                    <button type="submit"
-                        class="bg-[#015057] text-white py-4 rounded-md gap-4 flex items-center 
-                    justify-center transition-all duration-500 hover:bg-[rgba(1,80,87,0.8)] hover:cursor-pointer hover:gap-6">Next
+                    <button type="submit" class="bg-[#015057] text-white py-4 rounded-md gap-4 flex items-center mt-4
+                    justify-center transition-all duration-500 hover:bg-[rgba(1,80,87,0.8)]
+                     hover:cursor-pointer hover:gap-6">Next
                         <p>
                             <i class="fa-solid fa-arrow-right">
                         </p></i>
@@ -75,24 +99,9 @@ if(isset($_POST['email'], $_POST['password'])){
     echo LoginController::login($input_email, $input_password);
 }
 ?>
-
-
                 <hr class="border-[#cccc]">
 
-                <p class="text-center text-[#707070]">or log in with</p>
 
-                <div class="flex flex-col gap-4 items-center justify-center">
-                    <button class="border border-solid border-[#cccc] py-4 px-8 w-full flex items-center
-                         justify-center hover:cursor-not-allowed "><img class="w-9 mr-4"
-                            src="../public/img/facebook.png" alt="">
-                        <p>Facebook</p>
-                    </button>
-                    <button class="border border-solid border-[#cccc] py-4 px-8 w-full flex 
-                        items-center justify-center hover:cursor-not-allowed"><img class="w-9 mr-4"
-                            src="../public/img/google.png" alt="">
-                        <p>Google</p>
-                    </button>
-                </div>
             </div>
 
 
@@ -105,8 +114,7 @@ if(isset($_POST['email'], $_POST['password'])){
                 <figure class="flex w-sm">
                     <img class="rounded-r-lg" src="../public/img/image-1.jpg" alt="">
                 </figure>
-                <a href="./landingPage.php" class="absolute text-4xl -top-4 -right-4
-                 text-white rounded-[50%] shadow-xl transition-all duration-200
+                <a href="./landingPage.php" class="absolute text-4xl -top-4 -right-4 text-white rounded-[50%] shadow-xl transition-all duration-200
                     hover:text-[#fdcd6c] hover:scale-110 hover:cursor-pointer">
                     <i class=" fa-solid fa-circle-xmark "></i>
                 </a>
